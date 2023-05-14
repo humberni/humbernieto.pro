13e6aebd3e0b0b1031572a87400c35618b94ad9b
Humber Sanchez Nieto <humbersnieto@gmail.com>
2023-05-14 18:44:56 -0500

    logo link home

--- src/components/HeroVideo.tsx
+++ src/components/HeroVideo.tsx
@@ -14,7 +14,10 @@ export default function HeroVideo() {
         className="absolute top-0 left-0 w-full h-full object-cover object-center opacity-40"
         poster="/images/preload.webp"
       >
-        <source src="https://storage.googleapis.com/projects-video/nieto-ia.mp4" type="video/mp4" />
+        <source
+          src="https://storage.googleapis.com/projects-video/nieto-ia.mp4"
+          type="video/mp4"
+        />
         Su navegador no soporta la etiqueta de vídeo.
       </video>
       <div className="relative h-screen mx-auto ">
@@ -25,15 +28,17 @@ export default function HeroVideo() {
           <p className="text-[#09E989] text-lg">{"Developer </>"}</p>
         </div>
         <div>
-          <Image
-            className="h-10 w-auto fixed top-0 left-0 right-0 pl-6 lg:pl-8 mt-6 lg:mt-8"
-            src="/images/logo/humber-green.svg"
-            alt="Logo - Humber Nieto"
-            width={300}
-            height={300}
-            quality={75}
-            sizes="100vw"
-          />
+          <Link href="/">
+            <Image
+              className="h-10 w-auto fixed top-0 left-0 right-0 pl-6 lg:pl-8 mt-6 lg:mt-8"
+              src="/images/logo/humber-green.svg"
+              alt="Logo - Humber Nieto"
+              width={300}
+              height={300}
+              quality={75}
+              sizes="100vw"
+            />
+          </Link>
         </div>
         <div className="px-6 sm:px-6 lg:px-8 absolute right-0 bottom-20 lg:bottom-8">
           <Link href="#about" passHref legacyBehavior>
